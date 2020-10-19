698d4c2aeb66b00276785f276c5ecd91eb78bec4
Niklas <n.hakansson@hotmail.com>
2020-10-19 15:47:50 +0200

    Created a spring project - pokedex

---
+++ src/main/java/com/assignment/individual/pokedex/PokedexIndividualAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.individual.pokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokedexIndividualAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokedexIndividualAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/individual/pokedex/PokedexIndividualAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.individual.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokedexIndividualAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
