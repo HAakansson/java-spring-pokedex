377a5edd296ba4f0504ed38b3e953b76a1c9d108
Niklas <n.hakansson@hotmail.com>
2020-11-01 17:28:02 +0100

    Started on security

---
+++ src/main/java/com/assignment/individual/pokedex/security/ApplicationSecurityConfig.java
@@ -0,0 +1,67 @@
+package com.assignment.individual.pokedex.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.assignment.individual.pokedex.security.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+  private final PasswordEncoder passwordEncoder;
+
+  @Autowired
+  public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
+
+  @Override // Basic authentication
+  protected void configure(HttpSecurity http) throws Exception {
+    http
+        .authorizeRequests()
+        .antMatchers(HttpMethod.GET, "/api/v1/pokemons/**").permitAll()
+        .antMatchers(HttpMethod.GET, "/api/v1/pokemons").permitAll()
+        .antMatchers(HttpMethod.GET, "/api/v1/**").hasAnyRole("USER", "ADMIN")
+        .and()
+        .httpBasic();
+  }
+
+  @Override
+  @Bean
+  protected UserDetailsService userDetailsService() {
+    UserDetails niklasUser = User.builder()
+        .username("niklas")
+        .password(passwordEncoder.encode("password"))
+        .roles(USER.name()) //ROLE_USER
+        .build();
+
+    UserDetails mariaUser = User.builder()
+        .username("maria")
+        .password(passwordEncoder.encode("password"))
+        .roles(ADMIN.name())
+        .build();
+
+    UserDetails elsaUser = User.builder()
+        .username("elsa")
+        .password(passwordEncoder.encode("password"))
+        .build();
+
+    return new InMemoryUserDetailsManager(
+        niklasUser,
+        mariaUser,
+        elsaUser
+    );
+  }
+}
---
+++ src/main/java/com/assignment/individual/pokedex/security/ApplicationUserRole.java
@@ -0,0 +1,22 @@
+package com.assignment.individual.pokedex.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static com.assignment.individual.pokedex.security.ApplicationUserPermisson.*;
+
+public enum ApplicationUserRole {
+  USER(Sets.newHashSet()),
+  ADMIN(Sets.newHashSet(API_READ, API_WRITE, USER_READ, USER_WRITE));
+
+  private final Set<ApplicationUserPermisson> permissons;
+
+  ApplicationUserRole(Set<ApplicationUserPermisson> permissons) {
+    this.permissons = permissons;
+  }
+
+  public Set<ApplicationUserPermisson> getPermissons() {
+    return permissons;
+  }
+}
